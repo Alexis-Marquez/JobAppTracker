e49b9e2db698504b6385e379aacf248b5e7de7ac
Alexis Marquez <alexismarq337@gmail.com>
2026-03-09 15:21:30 -0600

    Add fix to database connection

--- backend/JobTracker/settings.py
+++ backend/JobTracker/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%$ri06ws*wvtd9#qtb)%)qnamp(#z)o%t91mefqd3vnl_2!&0i'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'sjhfoweryiu1243706324-234965')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,12 +82,15 @@ WSGI_APPLICATION = 'JobTracker.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
-}
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+    DATABASES['default']['CONN_MAX_AGE'] = 600
+else:
+    raise ValueError("DATABASE_URL environment variable is empty or not sets")
 
 
 # Password validation
